3975a3b26260119b319aab010731005b3ae059ba
Hawani Mohamad <4221003091D@student.unisel.edu.my>
2025-10-23 05:46:25 +0800

    establish index.html

--- frontend/views.py
+++ frontend/views.py
@@ -1,8 +1,4 @@
-from django.http import HttpResponse
-import datetime
+from django.shortcuts import render
 
-
-def current_datetime(request):
-    now = datetime.datetime.now()
-    html = '<html lang="en"><body>It is now %s.</body></html>' % now
-    return HttpResponse(html)
+def members(request):
+  return render(request, 'frontend/index.html')
